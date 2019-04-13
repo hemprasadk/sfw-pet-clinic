82d0c86fd6775de56b2a0e469ff9b95bbd1c34af
hekenderi <hemprasadk@gmail.com>
2019-04-13 15:56:56 -0400

    Inital project Add

---
+++ src/main/java/prasad/springframework/sfwpetclinic/SfwPetClinicApplication.java
@@ -0,0 +1,13 @@
+package prasad.springframework.sfwpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfwPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfwPetClinicApplication.class, args);
+    }
+
+}
