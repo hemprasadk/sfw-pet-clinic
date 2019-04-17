d41eeba448a9a08b46e32584d0769ee0fd4717ed
hekenderi <hemprasadk@gmail.com>
2019-04-17 15:53:23 -0400

    test

---
+++ pet-clinic-data/src/main/java/prasad/springframework/sfwpetclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package prasad.springframework.sfwpetclinic.model;
+
+public class Speciality extends BaseEntitiy{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
