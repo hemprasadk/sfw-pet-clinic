d69d14002bfa6d02cea1b585ec3c8edc8987c4fe
hekenderi <hemprasadk@gmail.com>
2019-04-17 16:31:11 -0400

    test

---
+++ pet-clinic-data/src/main/java/prasad/springframework/sfwpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package prasad.springframework.sfwpetclinic.services;
+
+
+import prasad.springframework.sfwpetclinic.model.PetType;
+
+public interface PetTypeService extends CurdService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/prasad/springframework/sfwpetclinic/services/map/PetTypeServiceServiceMap.java
@@ -0,0 +1,38 @@
+package prasad.springframework.sfwpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import prasad.springframework.sfwpetclinic.model.Pet;
+import prasad.springframework.sfwpetclinic.model.PetType;
+import prasad.springframework.sfwpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceServiceMap extends AbstractMapService<PetType, Long>  implements PetTypeService {
+
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType finById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+            super.deleteByid(aLong);
+    }
+}
